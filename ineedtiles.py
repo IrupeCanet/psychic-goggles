ecdf15fadf703ecaeb6da103b9ba3e8dc775059b
IrupeCanet <60444322+IrupeCanet@users.noreply.github.com>
2020-03-31 13:09:18 +0100

    Add files via upload

---
+++ ineedtiles.py
@@ -0,0 +1,12 @@
+# This program calculates how many tiles you will need
+# when tilling a wall or floor (in m2).
+
+length = 3.5
+width = 2.3
+
+area = length * width
+
+needed = area * 1.05
+
+print("You need", needed, "tiles in metres squared")
+
